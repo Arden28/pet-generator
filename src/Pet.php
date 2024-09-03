b7bf3f39cbc5f0403b4581d57f8ca9838c284354
Arden28 <laudbouetoumoussa@gmail.com>
2024-09-03 21:36:45 +0100

    Update the api call

--- src/Pet.php
+++ src/Pet.php
@@ -7,9 +7,7 @@ use Illuminate\Support\Facades\Http;
 class Pet{
 
     public function cat() {
-        $response = Http::withHeaders([
-            'x-api-key' => 'live_zi59OGBL2sLhoWx0eiUyAiGktcZFi82mmQaDobPcfvn6JEt56vOC9PSPVvwIFNvM'
-        ])->get('https://api.thecatapi.com/v1/images/search', [
+        $response = Http::get('https://api.thecatapi.com/v1/images/search?limit=10', [
             'limit' => 10
         ]);
 
